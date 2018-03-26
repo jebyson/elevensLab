716e2b37c686a020227101ebbb21ef3a16ae4d4a
Ebyson, Joshua <30394310+jebyson@users.noreply.github.com>
2018-03-26 08:35:31 -0400

    Update CardTester.java

--- src/com/company/CardTester.java
+++ src/com/company/CardTester.java
@@ -10,6 +10,7 @@ public class CardTester {
      * The main method in this class checks the Card operations for consistency.
      *	@param args is not used.
      */
+	
 
     public static void main(String[] args) {
 		Card firstOne = new Card("3", "Diamonds", 3);
