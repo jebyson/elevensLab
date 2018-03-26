54125070229895907b12afe7b8b2ae92c2f70453
Ebyson, Joshua <30394310+jebyson@users.noreply.github.com>
2018-03-26 08:35:37 -0400

    Update CardTester.java

--- src/com/company/CardTester.java
+++ src/com/company/CardTester.java
@@ -11,7 +11,6 @@ public class CardTester {
      *	@param args is not used.
      */
 	
-
     public static void main(String[] args) {
 		Card firstOne = new Card("3", "Diamonds", 3);
         Card secondOne = new Card("6", "Hearts", 6);
